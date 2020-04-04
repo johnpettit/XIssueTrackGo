1770c2c95ddc9ef80a1f051fd028251093b49803
John Pettit <john.pettit@cox.net>
2020-04-04 07:44:05 -0700

    Start hitting the DB

--- business/auth.go
+++ business/auth.go
@@ -1,18 +1,30 @@
 package business
 
 import (
+	"XIssueTrackGo/database"
 	"XIssueTrackGo/model"
+	"database/sql"
 	"errors"
 )
 
 //UserLogin login a user
 func UserLogin(auth model.AuthRequest) (model.Token, error) {
+	var user model.User
 	var token model.Token
-	if auth.Email == "john.pettit@cox.net" {
-		token.Token = "37425b789a27ecfa5e6fc59183ac34ca" //md5 of chuck111
+
+	result := database.DBSession.QueryRow("SELECT id, firstname, lastname, email FROM user WHERE email = ? and password = ?", auth.Email, auth.Password)
+
+	switch err := result.Scan(&user.UserID, &user.FirstName, &user.LastName, &user.Email); err {
+	case sql.ErrNoRows:
+		//no match    bad pw?   invalid email?
+		return token, errors.New("Email/Password not found")
+	case nil:
+		token.Token = "37425b789a27ecfa5e6fc59183ac34ca"
 		return token, nil
+	default:
+		//WHAT??
+		panic(err)
 	}
-	return token, errors.New("No Match")
 }
 
 //CheckToken see if token is valid
